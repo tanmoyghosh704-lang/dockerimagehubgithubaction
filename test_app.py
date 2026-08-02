83972d96d611d2c8ec51721306e3f303e92fa5fb
Taannoy <tanmoyghosh704@gmail.com>
2026-08-03 00:40:46 +0530

    test case revised

--- test_app.py
+++ test_app.py
@@ -7,5 +7,5 @@ client=TestClient(app)
 def test_server():
     response=client.get("/")
     assert response.status_code==200
-    assert response.json()=="hello!"
+    assert response.json()=="hello"
     
